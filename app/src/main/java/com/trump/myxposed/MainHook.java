ed4c97ac98e7a011c63c94170f058bb7ec20485b
wangyuan0217 <861316155@qq.com>
2017-07-18 13:05:10 +0800

    0.0.1

--- app/src/main/java/com/trump/myxposed/MainHook.java
+++ app/src/main/java/com/trump/myxposed/MainHook.java
@@ -1,17 +1,16 @@
 package com.trump.myxposed;
 
+import android.app.Application;
 import android.content.Context;
+import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.widget.TextView;
-
-import java.util.List;
 
 import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
@@ -24,68 +23,43 @@ import static de.robv.android.xposed.XposedHelpers.findClass;
  */
 public class MainHook implements IXposedHookLoadPackage, IXposedHookZygoteInit, IXposedHookInitPackageResources {
 
+    public static final String PACKAGE_WECHAT = "com.tencent.mm";
 
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
 //        addFlagInSystemBar(loadPackageParam);
-//        testMultiDexHook(loadPackageParam);
-        testHookRecruit(loadPackageParam);
+//        testHookRecruit(loadPackageParam);
 
-        if (!loadPackageParam.packageName.equals("com.tencent.mm"))
+        if (!loadPackageParam.packageName.equals(PACKAGE_WECHAT))
             return;
 
-        Log.d("xposed", "we are now in wechat");
+        Log.d("xposed", "in wechat now");
 
-        Class<?> pluginHelper = findClass("com.tencent.mm.ui.LauncherUI", loadPackageParam.classLoader);
-        findAndHookMethod(pluginHelper, "onCreateOptionsMenu", Menu.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                super.beforeHookedMethod(param);
-//                param.args[0] = null;
-            }
-
+        XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                super.afterHookedMethod(param);
-//
-//                Log.d("xposed", "come in success");
-//
-//                try {
-//                    Class c = loadPackageParam.classLoader.loadClass("com.tencent.mm.ui.conversation.d");
-//                    Field field = c.getDeclaredField("btS");
-//                    field.setAccessible(true);
-//
-//                    //param.thisObject 为执行该方法的对象，在这里指MainActivity
-//                    ImageView logo = (ImageView) field.get(param.args[1]);
-//                    ViewGroup.LayoutParams viewGroup = logo.getLayoutParams();
-//                    viewGroup.height = viewGroup.height / 2;
-//                    logo.setLayoutParams(viewGroup);
-//
-//                } catch (Exception e) {
-//
-//                }
+                ClassLoader cl = ((Context) param.args[0]).getClassLoader();
+                Class<?> hookclass = null;
+
+                try {
+                    Log.d("xposed", "load class start");
+                    hookclass = cl.loadClass(PACKAGE_WECHAT + ".ui.conversation.BizConversationUI");
+                } catch (Exception e) {
+                    Log.e("xposed", "find class error ", e);
+                    return;
+                }
+                Log.d("xposed", "find class success");
+                hookWechat(hookclass, "onCreate");
             }
         });
     }
 
-    public void testMultiDexHook(XC_LoadPackage.LoadPackageParam loadPackageParam) {
-        XposedHelpers.findAndHookMethod("android.app.Application", loadPackageParam.classLoader, "attach", Context.class, new XC_MethodHook() {
+    public void hookWechat(Class classLoader, String methodName) {
+        XposedHelpers.findAndHookMethod(classLoader, methodName, Bundle.class, new XC_MethodReplacement() {
             @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                Context context = (Context) param.args[0];
-                XposedHelpers.findAndHookMethod("com.tencent.mm.plugin.sns.ui.ap",
-                        context.getClassLoader(), "c",
-                        boolean.class, List.class, new XC_MethodHook() {
-                            @Override
-                            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                                List list = (List) param.args[1];
-                                XposedBridge.log("list:" + list);
-                                for (Object object : list) {
-                                    Class<?> aClass = object.getClass();
-                                    aClass.getFields();
-                                }
-                            }
-                        });
+            protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                Log.d("xposed", "hooked success");
+                return null;
             }
         });
     }
@@ -94,7 +68,7 @@ public class MainHook implements IXposedHookLoadPackage, IXposedHookZygoteInit, 
         if (!loadPackageParam.packageName.equals("com.libo.recruit"))
             return;
 
-        Log.d("xposed", "we are now in recruit update");
+        Log.d("xposed", "we are now in recruit");
 
 //        XposedHelpers.findAndHookMethod(“包名+类名”,
 //                lpparam.classLoader, “要hook的函数名称”, 第一个参数类型, 第二个参数类型….., new
@@ -112,7 +86,7 @@ public class MainHook implements IXposedHookLoadPackage, IXposedHookZygoteInit, 
                     Log.d("xposed", param.thisObject.toString());
 
                     TextView tv2 = (TextView) param.args[0];
-                    tv2.setText("Hook Success!!update");
+                    tv2.setText("Hook Success!");
 
                 } catch (Exception e) {
 
@@ -139,7 +113,6 @@ public class MainHook implements IXposedHookLoadPackage, IXposedHookZygoteInit, 
             Log.d("Xposed", "exception trump");
         }
     }
-
 
     @Override
     public void initZygote(StartupParam startupParam) throws Throwable {
