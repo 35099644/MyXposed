321f7d276d4ead9e7d71520fef71813788e42c30
wangyuan0217 <861316155@qq.com>
2017-07-19 16:30:40 +0800

    增加了屏蔽朋友圈的功能

--- app/src/main/java/com/trump/myxposed/MainHook.java
+++ app/src/main/java/com/trump/myxposed/MainHook.java
@@ -1,7 +1,9 @@
 package com.trump.myxposed;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
+import android.os.Bundle;
 import android.util.Log;
 
 import de.robv.android.xposed.IXposedHookInitPackageResources;
@@ -29,40 +31,70 @@ public class MainHook implements IXposedHookLoadPackage, IXposedHookZygoteInit, 
                     protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                         ClassLoader cl = ((Context) param.args[0]).getClassLoader();
 
-                        Class<?> hookclass = null;
-                        String className = PACKAGE_WECHAT + ".s.o";
-                        String fun = "eX";
+                        shieldSpecialChat(cl);
 
+                        shieldMoments(cl);
+                    }
+                });
+    }
+
+    private void shieldSpecialChat(ClassLoader cl) {
+        Class<?> hookclass = null;
+        String className = PACKAGE_WECHAT + ".s.o";
+        String fun = "eX";
+
+        try {
+            hookclass = cl.loadClass(className);
+        } catch (Exception e) {
+            Log.e("xposed", "find class error ", e);
+            return;
+        }
+
+        XposedHelpers.findAndHookMethod(hookclass, fun, String.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        super.afterHookedMethod(param);
+                        String paramStr = param.args[0].toString();
+
+                        //需要屏蔽的聊天对象的id  可以是好友、群、公众号、、、、、
+                        String orginStr1 = "2342477308@chatroom";  //鸡年行大运
+                        String orginStr2 = "1730967485@chatroom";  //苟富贵
+                        String orginStr3 = "1072389255@chatroom";  //秘密基地的id
+
+                        boolean finalBool = judgeArg(paramStr, orginStr1, orginStr2, orginStr3);
+                        param.setResult(finalBool);
+
+//                        Log.d("xposed", "入参----" + paramStr);
+//                        Log.d("xposed", "hook success!!!   return=" + finalBool);
+                    }
+                });
+    }
+
+    public void shieldMoments(final ClassLoader cl) {
+        Class<?> hookclass = null;
+        String className = PACKAGE_WECHAT + ".plugin.sns.ui.En_424b8e16";
+        String fun = "onCreate";
+
+        try {
+            hookclass = cl.loadClass(className);
+        } catch (Exception e) {
+            Log.e("xposed", "find class error ", e);
+            return;
+        }
+
+        XposedHelpers.findAndHookMethod(hookclass, fun,
+                Bundle.class, new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        super.afterHookedMethod(param);
                         try {
-                            Log.d("xposed", "load class start");
-                            hookclass = cl.loadClass(className);
+                            Activity context = (Activity) param.thisObject;
+                            context.finish();
                         } catch (Exception e) {
-                            Log.e("xposed", "find class error ", e);
-                            return;
+                            Log.e("xposed", "hook exception=" + e);
                         }
-
-                        Log.d("xposed", "find class success and start hook");
-
-                        XposedHelpers.findAndHookMethod(hookclass, fun, String.class,
-                                new XC_MethodHook() {
-                                    @Override
-                                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                                        super.afterHookedMethod(param);
-                                        String paramStr = param.args[0].toString();
-
-                                        //需要屏蔽的聊天对象的id  可以是好友、群、公众号、、、、、
-                                        String orginStr1 = "2342477308@chatroom";  //鸡年行大运
-                                        String orginStr2 = "1730967485@chatroom";  //苟富贵
-                                        String orginStr3 = "1072389255@chatroom";  //秘密基地的id
-
-                                        boolean finalBool = judgeArg(paramStr, orginStr1, orginStr2, orginStr3);
-                                        param.setResult(finalBool);
-
-                                        Log.d("xposed", "入参----" + paramStr);
-                                        Log.d("xposed", "hook success!!!   return=" + finalBool);
-                                    }
-                                });
-
+                        Log.d("xposed", "hook success");
                     }
                 });
     }
