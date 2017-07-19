b6d453cbc95eadafb6e1a008af5b6024d7509166
wangyuan0217 <861316155@qq.com>
2017-07-19 13:56:18 +0800

    1.0 功能：对微信主页聊天列表的特定聊天对象进行屏蔽
    待完善
    暂时写死的列表聊天对象id
    
    todo
    增加界面  用户可以自定义聊天对象

--- app/src/main/java/com/trump/myxposed/MainHook.java
+++ app/src/main/java/com/trump/myxposed/MainHook.java
@@ -2,21 +2,15 @@ package com.trump.myxposed;
 
 import android.app.Application;
 import android.content.Context;
-import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
 
 import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
-
-import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
-import static de.robv.android.xposed.XposedHelpers.findClass;
 
 /**
  * just for test temp
@@ -27,91 +21,63 @@ public class MainHook implements IXposedHookLoadPackage, IXposedHookZygoteInit, 
 
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
-//        addFlagInSystemBar(loadPackageParam);
-//        testHookRecruit(loadPackageParam);
-
         if (!loadPackageParam.packageName.equals(PACKAGE_WECHAT))
             return;
+        XposedHelpers.findAndHookMethod(Application.class,
+                "attach", Context.class, new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        ClassLoader cl = ((Context) param.args[0]).getClassLoader();
 
-        Log.d("xposed", "in wechat now");
+                        Class<?> hookclass = null;
+                        String className = PACKAGE_WECHAT + ".s.o";
+                        String fun = "eX";
 
-        XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                ClassLoader cl = ((Context) param.args[0]).getClassLoader();
-                Class<?> hookclass = null;
-
-                try {
-                    Log.d("xposed", "load class start");
-                    hookclass = cl.loadClass(PACKAGE_WECHAT + ".ui.conversation.BizConversationUI");
-                } catch (Exception e) {
-                    Log.e("xposed", "find class error ", e);
-                    return;
-                }
-                Log.d("xposed", "find class success");
-                hookWechat(hookclass, "onCreate");
-            }
-        });
-    }
-
-    public void hookWechat(Class classLoader, String methodName) {
-        XposedHelpers.findAndHookMethod(classLoader, methodName, Bundle.class, new XC_MethodReplacement() {
-            @Override
-            protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
-                Log.d("xposed", "hooked success");
-                return null;
-            }
-        });
-    }
-
-    public void testHookRecruit(XC_LoadPackage.LoadPackageParam loadPackageParam) {
-        if (!loadPackageParam.packageName.equals("com.libo.recruit"))
-            return;
-
-        Log.d("xposed", "we are now in recruit");
-
-//        XposedHelpers.findAndHookMethod(“包名+类名”,
-//                lpparam.classLoader, “要hook的函数名称”, 第一个参数类型, 第二个参数类型….., new
-//                XC_MethodHook()
-
-        Class<?> pluginHelper = findClass("com.libo.recruit.ui.activity.AboutUsActivity", loadPackageParam.classLoader);
-        findAndHookMethod(pluginHelper, "setText", TextView.class, new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                super.afterHookedMethod(param);
-//                param.args[0]：得到被拦截方法的第一个参数，返回值是Object
-//                param.getResult()：得到被拦截方法的执行结果，返回值是Object
-                try {
-                    Log.d("xposed", param.args[0].toString());
-                    Log.d("xposed", param.thisObject.toString());
-
-                    TextView tv2 = (TextView) param.args[0];
-                    tv2.setText("Hook Success!");
-
-                } catch (Exception e) {
-
-                }
-            }
-        });
-    }
-
-    public void addFlagInSystemBar(XC_LoadPackage.LoadPackageParam loadPackageParam) {
-        if (!loadPackageParam.packageName.equals("com.android.systemui"))
-            return;
-
-        try {
-            findAndHookMethod("com.android.systemui.statusbar.policy.Clock",
-                    loadPackageParam.classLoader, "updateClock", new XC_MethodHook() {
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                            TextView tv = (TextView) param.thisObject;
-                            String text = tv.getText().toString();
-                            tv.setText("Trump " + text);
+                        try {
+                            Log.d("xposed", "load class start");
+                            hookclass = cl.loadClass(className);
+                        } catch (Exception e) {
+                            Log.e("xposed", "find class error ", e);
+                            return;
                         }
-                    });
-        } catch (Exception e) {
-            Log.d("Xposed", "exception trump");
+
+                        Log.d("xposed", "find class success and start hook");
+
+                        XposedHelpers.findAndHookMethod(hookclass, fun, String.class,
+                                new XC_MethodHook() {
+                                    @Override
+                                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                                        super.afterHookedMethod(param);
+                                        String paramStr = param.args[0].toString();
+
+                                        //需要屏蔽的聊天对象的id  可以是好友、群、公众号、、、、、
+                                        String orginStr1 = "2342477308@chatroom";  //鸡年行大运
+                                        String orginStr2 = "1730967485@chatroom";  //苟富贵
+                                        String orginStr3 = "1072389255@chatroom";  //秘密基地的id
+
+                                        boolean finalBool = judgeArg(paramStr, orginStr1, orginStr2, orginStr3);
+                                        param.setResult(finalBool);
+
+                                        Log.d("xposed", "入参----" + paramStr);
+                                        Log.d("xposed", "hook success!!!   return=" + finalBool);
+                                    }
+                                });
+
+                    }
+                });
+    }
+
+    public boolean judgeArg(String param, String... args) {
+        boolean returnBool = false;
+
+        for (String arg : args) {
+            if (arg.equals(param)) {
+                returnBool = true;
+                break;
+            }
         }
+
+        return returnBool;
     }
 
     @Override
